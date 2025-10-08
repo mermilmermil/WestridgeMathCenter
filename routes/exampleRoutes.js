1375a92e15fc6eca368d3b7088e8b4a3266cda73
Caroline <cm26452@westridge.org>
2025-10-07 22:07:06 -0400

    route mean

--- routes/exampleRoutes.js
+++ routes/exampleRoutes.js
@@ -17,7 +17,7 @@ router.get('/teacher/deleteAbs/:idFellow/:idName', delAbsent)
 router.post('/scheduleMeeting', scheduleMeeting)
 router.post('/removeMeeting', removeMeeting)
 
-router.get('/populate1234', assignmentCsvToJson)
+router.get('/populate', assignmentCsvToJson)
 
 
 
